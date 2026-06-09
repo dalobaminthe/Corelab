9705ff5e67877de37ee753568a3c8eeea17a7bdf
daloba-epitech <daloba.minthe@epitech.eu>
2026-06-09 13:25:54 +0200

    feat(notifications): add node-cron job for lesson availability notifications

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,26 @@
 import dotenv from 'dotenv'
 dotenv.config()
+import express from 'express'
+import cors from 'cors'
 import mongoose from 'mongoose'
-import app from './app.js'
+import router from './routes/index.js'
+import errorHandler from './middleware/errorHandler.js'
+import { startNotificationJob } from './jobs/notificationJob.js'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.text({ type: 'text/csv' }))
+app.use('/api', router)
+app.use(errorHandler)
+
+app.get('/health', (req, res) => res.json({ status: 'ok' }))
 
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('MongoDB connected'))
+  .then(() => {
+    console.log('MongoDB connected')
+    startNotificationJob()
+  })
   .catch(err => console.error('MongoDB error:', err))
 
 app.listen(4242, () => console.log('Server running on port 4242'))
